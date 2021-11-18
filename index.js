fa75f3988d248a8fff1a06b702b82dfa92f275d8
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-11-17 20:41:09 -0500

    Create default json file

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const mongoose = require('mongoose');
+const connectDB = require('./startup/db');
 
-mongoose
-    .connect('mongodb+srv://maxwellsebas:Bruce143@cluster0.vyeb4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log(`Could not connect to MongoDB. ERROR: ${err}`))
+connectDB();
