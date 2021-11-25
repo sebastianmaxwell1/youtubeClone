3b3c3a2c2419a0f93ecf36011b72c6770202d444
Sebastian Maxwell <sebastianrmaxwell@gmail.com>
2021-11-24 19:18:23 -0500

    Re-create frontend folder

---
+++ Frontend/youtube-clone/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
+
+const jsxElement = <h1> YouTube Clone</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(jsxElement, document.getElementById('root'));
